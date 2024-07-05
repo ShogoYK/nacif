03243fe50a4ed87e8bd19c9fa46a52f8eac21934
ShogoYK <shogonicolas@gmail.com>
2024-07-05 01:54:32 -0400

    fix: Fixing addContact validation where the name should be unique

--- backend/src/Middlewares/ValidationMiddleware.js
+++ backend/src/Middlewares/ValidationMiddleware.js
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
 class ValidationMiddleware {
 
   validateLogin(req, res, next) {
@@ -10,14 +12,25 @@ class ValidationMiddleware {
     next();
   }
 
-  newContact(req, res, next) {
+  async newContact(req, res, next) {
 
     if (!req.body.name || !req.body.number) {
       return res.status(500).json({
         message: "Please fill in all the fields"
       })
     }
+    
+    const prisma = new PrismaClient();
+    const contact = await prisma.contact.findFirst({
+      where: {name: req.body.name, user_id: req.user.id}
+    })
 
+    if(contact) {
+      return res.status(401).json({
+        message: "You already have a contact with this name added!"
+      })
+    }
+    
     next()
   }
 }
