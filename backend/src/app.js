589ad820320d4dbf7edde4fb185c2c929beedf26
ShogoYK <shogonicolas@gmail.com>
2024-07-05 04:24:42 -0400

    build: Adding cors to the API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express"
+import cors from 'cors'
 import ContactsRouter from "./Routes/ContactsRouter.js"
 import AuthRouter from "./Routes/AuthRouter.js"
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 app.use("/contacts", ContactsRouter)
 app.use("/auth", AuthRouter)
 
